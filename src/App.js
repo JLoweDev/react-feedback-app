7c817c4993b1a77451e228c098833fb4789f7944
James <67014970+JLoweDev@users.noreply.github.com>
2022-03-13 19:20:43 +0000

    Added FeedbackData.js

--- src/App.js
+++ src/App.js
@@ -1,25 +1,10 @@
 import { useState } from 'react'
 import Header from './components/Header'
 import FeedbackItem from './components/FeedbackItem'
+import FeedbackData from './data/FeedbackData'
 
 function App() {
-    const [feedback, setFeedback] = useState([
-        {
-            id: 1,
-            rating: 7,
-            text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio necessitatibus totam aperiam sint labore! Nihil animi voluptas esse quae harum!'
-        },
-        {
-            id: 2,
-            rating: 9,
-            text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo officia, accusantium enim rerum reprehenderit adipisci. Rerum distinctio doloremque maxime amet sit optio. Quod consequuntur ipsa debitis neque sit! Perspiciatis quam deleniti adipisci nesciunt libero similique.'
-        },
-        {
-            id: 3,
-            rating: 4,
-            text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, odit!'
-        },
-    ])
+    const [feedback, setFeedback] = useState(FeedbackData)
 
     return (
         <>
---
+++ src/data/FeedbackData.js
@@ -0,0 +1,19 @@
+const FeedbackData = [
+    {
+        id: 1,
+        rating: 7,
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio necessitatibus totam aperiam sint labore! Nihil animi voluptas esse quae harum!'
+    },
+    {
+        id: 2,
+        rating: 9,
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo officia, accusantium enim rerum reprehenderit adipisci. Rerum distinctio doloremque maxime amet sit optio. Quod consequuntur ipsa debitis neque sit! Perspiciatis quam deleniti adipisci nesciunt libero similique.'
+    },
+    {
+        id: 3,
+        rating: 4,
+        text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, odit!'
+    },
+]
+
+export default FeedbackData
