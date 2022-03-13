f132cab97a47a2f67d44cbbc32e0768d94ec6e81
James <67014970+JLoweDev@users.noreply.github.com>
2022-03-13 22:47:59 +0000

    added delete functionality

--- src/components/FeedbackItem.js
+++ src/components/FeedbackItem.js
@@ -1,14 +1,15 @@
+import { FaTimes } from 'react-icons/fa'
 import React from 'react'
 import PropTypes from 'prop-types'
 import Card from './shared/Card'
 
-function FeedbackItem({ rating, text }) {
-    
+function FeedbackItem({ item, handleDelete }) {
 
     return (
         <Card>
-            <div className="num-display">{rating}</div>
-            <div className="text-display">{text}</div>
+            <div className="num-display">{item.rating}</div>
+            <button className="close" onClick={() => handleDelete(item.id)}><FaTimes color="purple"/></button>
+            <div className="text-display">{item.text}</div>
         </Card>
     )
 }
