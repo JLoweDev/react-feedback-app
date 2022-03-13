bdeef71060cfeec1b9a06a1dffc09037a6c7f1c5
James <67014970+JLoweDev@users.noreply.github.com>
2022-03-13 19:07:29 +0000

    added feedback state w/ placeholders

--- src/App.js
+++ src/App.js
@@ -1,7 +1,26 @@
+import { useState } from 'react'
 import Header from './components/Header'
 import FeedbackItem from './components/FeedbackItem'
 
 function App() {
+    const [feedback, setFeedback] = useState([
+        {
+            id: 1,
+            rating: 7,
+            text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Optio necessitatibus totam aperiam sint labore! Nihil animi voluptas esse quae harum!'
+        },
+        {
+            id: 2,
+            rating: 9,
+            text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Quo officia, accusantium enim rerum reprehenderit adipisci. Rerum distinctio doloremque maxime amet sit optio. Quod consequuntur ipsa debitis neque sit! Perspiciatis quam deleniti adipisci nesciunt libero similique.'
+        },
+        {
+            id: 3,
+            rating: 4,
+            text: 'Lorem ipsum dolor sit amet consectetur adipisicing elit. Officia, odit!'
+        },
+    ])
+
     return (
         <>
             <Header text="Hello World" />
