e1beb2190da581b01324fa3be447f61aa0d1eaa0
James <67014970+JLoweDev@users.noreply.github.com>
2022-03-13 19:35:39 +0000

    FeedbackList.js added

--- src/components/FeedbackItem.js
+++ src/components/FeedbackItem.js
@@ -1,12 +1,10 @@
 import React from 'react'
-import { useState } from 'react'
 
-function FeedbackItem() {
-    const [rating, setRating] = useState(7)
-    const [text, setText] = useState('This is text')
+function FeedbackItem({ rating, text, key }) {
+    
 
     return (
-        <div className='card'>
+        <div className='card' key={key}>
             <div className="num-display">{rating}</div>
             <div className="text-display">{text}</div>
         </div>
