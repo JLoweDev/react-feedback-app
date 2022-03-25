b97168249d66327aa058cad1c27816e77b6ac6ea
James <67014970+JLoweDev@users.noreply.github.com>
2022-03-25 16:06:36 +0000

    added edit button to FeedbackItem.js

--- src/context/FeedbackContext.js
+++ src/context/FeedbackContext.js
@@ -11,22 +11,37 @@ export const FeedbackProvider = ({children}) => {
             rating: 10,
         }
     ])
+    const [feedbackEdit, setFeedbackEdit] = useState({
+        item: {},
+        edit: false
+    })
 
+    // Delete feedback
     const deleteFeedback = (id) => {
         if(window.confirm("Are you sure you want to delete?")) {
             setFeedback(feedback.filter((item) => item.id !== id))
         }
     }
 
+    // Add feedback
     const addFeedback = (newFeedback) => {
         newFeedback.id = uuidv4()
         setFeedback([newFeedback, ...feedback])
+    }
+
+    // Set item to be updated
+    const editFeedback = (item) => {
+        setFeedbackEdit({
+            item,
+            edit: true,
+        })
     }
 
     return <FeedbackContext.Provider value={{
         feedback,
         deleteFeedback,
         addFeedback,
+        editFeedback,
     }}>
         {children}
     </FeedbackContext.Provider>
